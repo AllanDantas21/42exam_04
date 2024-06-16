5e3119ccba2994111f96a40ed21ebb68c1cb710f
Allandantas21 <Allandantas21.ad@gmail.com>
2024-06-16 01:44:10 -0300

    new attemp

--- microshell.c
+++ microshell.c
@@ -1,62 +1,54 @@
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
-/*not needed in exam, but necessary if you want to use this tester:
-https://github.com/Glagan/42-exam-rank-04/blob/master/microshell/test.sh*/
-// #ifdef TEST_SH
-// # define TEST		1
-// #else
-// # define TEST		0
-// #endif
-
-void	ft_putstr_fd2(char *str, char *arg)
+void	ft_error(char *err, char *arg)
 {
-	while (*str)
-		write(2, str++, 1);
+	while(*err)
+		write(2, err++, 1);
 	if (arg)
 		while(*arg)
 			write(2, arg++, 1);
 	write(2, "\n", 1);
 }
 
-void ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+char	ft_exec(char **argv, int i, int tmp_fd, char **env)
 {
 	argv[i] = NULL;
 	dup2(tmp_fd, STDIN_FILENO);
 	close(tmp_fd);
 	execve(argv[0], argv, env);
-	ft_putstr_fd2("error: cannot execute ", argv[0]);
+	ft_error("error: cannot execute ", argv[0]);
 	exit(1);
 }
 
-int	main(int argc, char *argv[], char *env[])
+int	main(int argc, char **argv, char **env)
 {
-	int	i;
-	int fd[2];
 	int tmp_fd;
-	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+	int fd[2];
+	int i;
+	(void)argc;
 
 	i = 0;
 	tmp_fd = dup(STDIN_FILENO);
-	while (argv[i] && argv[i + 1]) //check if the end is reached
+	while (argv[i] && argv[i + 1])
 	{
-		argv = &argv[i + 1];	//the new argv start after the ; or |
+		argv = &argv[i + 1];
 		i = 0;
 		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0) //cd
+		if (strcmp(argv[0], "cd") == 0)
 		{
 			if (i != 2)
-				ft_putstr_fd2("error: cd: bad arguments", NULL);
+				ft_error("error: cd: bad arguments", NULL);
 			else if (chdir(argv[1]) != 0)
-				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+				ft_error("error: cd: cannot change directory to", argv[1]);
 		}
-		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
 		{
-			if ( fork() == 0)
-				ft_execute(argv, i, tmp_fd, env);
+			if ( fork() == 0 )
+				ft_exec(argv, i, tmp_fd, env);
 			else
 			{
 				close(tmp_fd);
@@ -65,15 +57,15 @@ int	main(int argc, char *argv[], char *env[])
 				tmp_fd = dup(STDIN_FILENO);
 			}
 		}
-		else if(i != 0 && strcmp(argv[i], "|") == 0) 
+		else if (i != 0 && strcmp(argv[i], "|") == 0)
 		{
 			pipe(fd);
-			if ( fork() == 0)
+			if ( fork() == 0 )
 			{
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[0]);
 				close(fd[1]);
-				ft_execute(argv, i, tmp_fd, env);
+				ft_exec(argv, i, tmp_fd, env);
 			}
 			else
 			{
@@ -86,3 +78,4 @@ int	main(int argc, char *argv[], char *env[])
 	close(tmp_fd);
 	return (0);
 }
+
